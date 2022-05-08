dfed37e950acad79227743b50f4baf6a7aa159d4
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-08 00:10:19 -0700

    Product Details Section Done

--- src/Pages/ProductDetails/ProductDetails.js
+++ src/Pages/ProductDetails/ProductDetails.js
@@ -1,24 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-    const { id } = useParams();
-    const [product, setProduct] = useState();
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
 
-    useEffect(()=>{
-        const url = `http://localhost:5000/products/${id}`;
-        console.log(id);
+  useEffect(() => {
+    const url = `http://localhost:5000/products/${id}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, [id]);
+
+  const [quantity, setQuantity] = useState(1)
+  const handleDecrement = () => {
+      setQuantity(quantity - 1)
+      console.log(quantity);
     
-    
-    return (
-        <div>
-            <h2>this is product detail {id}</h2>
-        </div>
-    );
+  }
+  
+  return (
+    <div className="d-flex justify-content-center">
+      <Card style={{ width: "20rem" }}>
+        <Card.Img variant="top" src={product.picture} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>Price: ${product.price}</Card.Text>
+          <Card.Text>Quantity: {quantity}</Card.Text>
+          <Card.Text>{product.description}</Card.Text>
+          <Card.Text>Supplier: {product.supplierName}</Card.Text>
+          <button onClick={handleDecrement}>Delivered</button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default ProductDetails;
